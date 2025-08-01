c2990f6c8c717af013008cbfea960eace67f1252
mohamedamine.riahi <mohamedamine.riahi@amaris.com>
2025-08-01 10:35:40 +0100

    faire un padding dans le dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -9,7 +9,7 @@ import { RevenueStreamWidget } from './components/revenuestreamwidget';
     selector: 'app-dashboard',
     imports: [StatsWidget, RecentSalesWidget, RevenueStreamWidget],
     template: `
-        <div class="grid grid-cols-12 gap-8 mt-10">
+        <div class="grid grid-cols-12 gap-8 mt-10 p-5">
             <app-stats-widget class="contents" />
             <div class="col-span-12 xl:col-span-6">
                 <app-recent-sales-widget />
